d8515389a4d964ec542933662b4dcc09bb3f754c
Olga-Zivenko <108621714+Olga-Zivenko@users.noreply.github.com>
2022-09-20 00:16:10 +0200

    tried to do better

--- src/Search.js
+++ src/Search.js
@@ -5,11 +5,12 @@ import axios from "axios";
 export default function Search() {
   const [city, setCity] = useState("");
   const [loaded, setLoaded] = useState(false);
-  let [weather, setWeather] = useState({});
+  let [weather, setWeather] = useState("");
 
   function showWeather(response) {
     setLoaded(true);
     setWeather({
+        city: response.data.name,
       temperature: response.data.main.temp,
       description: response.data.weather[0].main,
       humidity: response.data.main.humidity,
@@ -29,40 +30,46 @@ export default function Search() {
   function updateCity(event) {
     setCity(event.target.value);
   }
+
   let form = (
-    <form onSubmit={handleSearch}>
-      <input type="search" placeholder="Type a city" onChange={updateCity} />
-      <input type="submit" value="Search" />
+    <form className="search-form"
+    onSubmit={handleSearch}>
+      <input type="search" placeholder="Type a city" onChange={updateCity} className="searchinput" />
+      <input type="submit" value="Search" className="searchButton"  />
     </form>
   );
+
   if (loaded) {
     return (
       <div>
         {form}
-        <ul>
-            <li className="headcity">${city}</li>
-            <li className="description">{weather.description}</li>
+         <ul>
+        <div className="row">
+    <div className="col-md-6">
+          <li className="headCity">{weather.city}</li>
+        <li className="sky">
+            <img src={weather.icon} alt="weather icon" width="70px"/>
+          </li>
+          </div>
+    <div className="col-md-6">
+          <li className="temperature">{Math.round(weather.temperature)}°C</li>
+          <div className="weatherInfo">
+            <li>Wind: {Math.round(weather.wind)} km/h</li>
+          <li>Humidity: {Math.round(weather.humidity)} %</li>
+          <li className="Description">{weather.description}</li>
+         </div>
+          </div>
+          </div>
         </ul>
-          <div className="row">
-            <div className="col-6">
-              <div className="sky">
-              <img src={weather.icon} alt="Weather icon" width="42" />
-                  <span className="float-left">
-                  <span className="temperature">{Math.round(weather.temperature)}</span>
-                  <span className="units">°C</span>
-                </span>
-              </div>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>Humidity: {weather.humidity}%</li>
-                <li>Wind: {Math.round(weather.wind)} km/h</li>
-              </ul>
-            </div>
-          </div>
-          </div>
+                   </div>
                    );
   } else {
-    return form;
+  
+    return form;}
   }
-}
+    
+    
+    
+   
+
+    
